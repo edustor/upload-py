66b7762468673cee1b8b0ed2140274b660606a6b
Dmitry Romanov <me@wutiarn.ru>
2017-04-20 10:14:44 +0300

    Get uploaded file

--- upload/web.py
+++ upload/web.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, request, abort
 from commons_auth.decorator import requires_scope
 app = Flask(__name__)
+app.config["MAX_CONTENT_LENGTH"] = 50 * 1024 ** 2
 
 
 @app.route("/api/v1/upload/pages", methods=["POST"])
-@requires_scope("upload2", "internal")
+@requires_scope("upload")
 def upload_pages():
+    if "file" not in request.files:
+        abort(400, "No file field in request")
+    file = request.files["file"]
     return "OK"
