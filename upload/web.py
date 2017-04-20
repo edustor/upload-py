610e72caccbe450c0c1549ff9279c7a401ff29b6
Dmitry Romanov <me@wutiarn.ru>
2017-04-20 10:48:59 +0300

    Publish rabbit message

--- upload/web.py
+++ upload/web.py
@@ -4,6 +4,8 @@ import uuid
 from flask import Flask, request, abort
 from gridfs import GridFSBucket
 from pymongo import MongoClient
+from datetime import datetime
+import pika
 
 from commons_auth.decorator import requires_scope
 
@@ -13,6 +15,9 @@ app.config["MAX_CONTENT_LENGTH"] = 50 * 1024 ** 2
 mongo = MongoClient()
 gridfs = GridFSBucket(mongo['edustor-files'], bucket_name="pages-uploads")
 
+rabbit_connection = pika.BlockingConnection(pika.ConnectionParameters())
+rabbit_channel = rabbit_connection.channel()
+
 
 @app.route("/api/v1/upload/pages", methods=["POST"])
 @requires_scope("upload")
@@ -21,7 +26,17 @@ def upload_pages():
         abort(400, "No file field in request")
     file = request.files["file"]
     upload_id = str(uuid.uuid4())
-    gridfs.upload_from_stream("upload-{}.pdf".format(upload_id), file.stream)
+    # gridfs.upload_from_stream("upload-{}.pdf".format(upload_id), file.stream)
+
+    event = {
+        "upload_id": upload_id,
+        "uploader_id": "",
+        "timestamp": int(datetime.now().timestamp()),
+        "targetLessonId": None
+    }
+
+    rabbit_channel.basic_publish("internal.edustor", "uploaded.pdf.pages.processing", json.dumps(event))
+
     return json.dumps({
         "upload_id": upload_id
     })
