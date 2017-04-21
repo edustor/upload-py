3cf1904f6b7c92d400971729171887b176d0857d
Dmitry Romanov <me@wutiarn.ru>
2017-04-21 09:54:53 +0300

    Declare exchange on start

--- upload/__init__.py
+++ upload/__init__.py
@@ -1,9 +1,15 @@
 import os
 
-import pika
+import pika.channel
+from pika.adapters.blocking_connection import BlockingChannel
 from pymongo import MongoClient
 
 mongo = MongoClient(os.environ["edustor.mongo.uri"])
 
 rabbit_connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ["edustor.rabbit.host"]))
 rabbit_channel = rabbit_connection.channel()
+
+assert isinstance(rabbit_channel, BlockingChannel)
+rabbit_channel.exchange_declare(exchange="internal.edustor",
+                                exchange_type="topic",
+                                durable=True)
