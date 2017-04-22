5d150b397ece593a51a5a12f4987752ae961bd52
Dmitry Romanov <me@wutiarn.ru>
2017-04-22 18:37:29 +0300

    Fix uwsgi startup

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ logging.basicConfig(level=logging.WARN, format='{asctime} {levelname} --- {name:
 for logger_name in ["edustor_upload", "werkzeug"]:
     logging.getLogger(logger_name).setLevel(logging.INFO)
 
+from edustor_upload.web import app
+
 if __name__ == '__main__':
-    from edustor_upload.web import app
     app.run(host="0.0.0.0")
